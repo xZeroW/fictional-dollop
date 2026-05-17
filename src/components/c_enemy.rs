c1a9bee5e0321194ddf7432ddb13b2f42a73e716
Victor <victor@192.168.80.32>
2026-05-17 05:20:11 -0300

    behavior component

--- src/components/c_enemy.rs
+++ src/components/c_enemy.rs
@@ -3,7 +3,7 @@ use bevy::prelude::Component;
 use crate::components::{Damage, Health, Movement};
 
 #[derive(Component, Default)]
-#[require(Health, Movement, Damage)]
+#[require(Health, Movement, Damage, Behavior)]
 pub struct Enemy {
     pub enemy_type: String,
 }
@@ -12,4 +12,12 @@ impl Enemy {
     pub fn new(enemy_type: String) -> Self {
         Enemy { enemy_type }
     }
+}
+
+#[derive(Component, Default)]
+pub enum Behavior {
+    #[default]
+    Wandering,
+    FollowAndAttack,
+    Coward,
 }
